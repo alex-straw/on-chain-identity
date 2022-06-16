1723c666983d988eddda198f6454e366e7d2873e
alex-straw <as17163@bristol.ac.uk>
2022-06-16 16:21:06 +0100

    fixed several syntax errors

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -11,24 +11,25 @@ contract Identity {
     address public owner;
     string public email;
     bytes32 private hash_pass;
-    bool verified = False;
+    bool public verified = false;
 
-    constructor (address _owner, string _email, bytes32 _hash_pass){
+    constructor (address _owner, string memory _email, bytes32 _hash_pass){
         owner = _owner;
         email = _email;
         hash_pass = _hash_pass;
     }
     
-    function verify_owner(string password) is_owner public {
-        require(bytes32(keccak256(abi.encode(password))) == hash_pass, "Incorrect password.")
-        verified = True;
+    function verify_owner(string memory password) onlyOwner public {
+        require(bytes32(keccak256(abi.encode(password))) == hash_pass, "Incorrect password.");
+        verified = true;
     } 
 
     function hash(string memory _text, uint _num, address _addr) public pure returns (bytes32) {
         return keccak256(abi.encode(_text, _num, _addr));
     }
 
-    modifier is_owner {
-        require(owner==msg.sender, "Only the owner can call this function.")
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can call this function.");
+            _;
     }
 }
