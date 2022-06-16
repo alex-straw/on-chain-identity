8111b9b83efa6c61297f902b4189a8fce42a6858
alex-straw <as17163@bristol.ac.uk>
2022-06-16 18:07:30 +0100

    removed owner check now that owner is just set to msg.sender

--- test/Identity.js
+++ test/Identity.js
@@ -1,28 +1,20 @@
 const { expect } = require('chai');
 const { ethers } = require("hardhat");
 
-let _owner = "0xF1C37BC188643DF4Bf15Fd437096Eb654d30abc1"
 let _email = "as17163@bristol.ac.uk"
 let _password = "testing"
-let _hashed = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_password))
+let _hashed_password = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_password))
 
-describe("Identity Contract", function () {
+describe("TEST: Identity Contract Deployment and Initialisation", function () {
     
     before(async function () {
-
         Identity = await ethers.getContractFactory('Identity');
         identity = await Identity.deploy(
-            _owner,
             _email,
-            _hashed,
+            _hashed_password,
         );
 
         await identity.deployed();
-    });
-
-    it("Should have the correct owner", async function() {
-        let owner = await identity.owner.call();
-        expect(owner).to.equal(_owner);
     });
 
     it("Should be unverified", async function() {
