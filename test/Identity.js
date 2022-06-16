6e15d8d46692b5159d71d010b71cafa7a1855382
alex-straw <as17163@bristol.ac.uk>
2022-06-16 17:37:32 +0100

    working initial test, properly packing the password as solidity expects

--- test/Identity.js
+++ test/Identity.js
@@ -1,18 +1,30 @@
-const { expect } = require("chai");
+const { expect } = require('chai');
+const { ethers } = require("hardhat");
 
+let _owner = "0xF1C37BC188643DF4Bf15Fd437096Eb654d30abc1"
+let _email = "as17163@bristol.ac.uk"
+let _password = "testing"
+let _hashed = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_password))
 
 describe("Identity Contract", function () {
-
+    
     before(async function () {
 
         Identity = await ethers.getContractFactory('Identity');
         identity = await Identity.deploy(
             _owner,
             _email,
-            _hashpass,
+            _hashed,
         );
 
         await identity.deployed();
     });
+
+    it("Should have the correct owner", async function() {
+        let owner = await identity.owner.call();
+        expect(owner).to.equal(_owner);
+    });
+
+    it("Should be unverified")
 
 });
